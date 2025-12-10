fe9eb1533604150390d49ff0fcfde6dec8d2d5eb
caole <3258932405@qq.com>
2025-12-10 23:48:26 +0800

    feat，chore（particalbackground）:引入了Particles库，重写了粒子背景，增加了可交互背景功能

--- honor-of-kings-vue/src/main.js
+++ honor-of-kings-vue/src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // 引入路由配置
+import Particles from "@tsparticles/vue3";
+import { loadSlim } from "@tsparticles/slim";
 
 const app = createApp(App)
 
 app.use(router) // 使用路由
+app.use(Particles, {
+  init: async engine => {
+    await loadSlim(engine);
+  },
+});
+
 app.mount('#app')
